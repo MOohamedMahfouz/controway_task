a2ee06ef6d166ccffb6647f998c678fb7983b1f1
MOohamedMahfouz <mouhamadmahfouz20@gmail.com>
2023-04-29 15:13:43 +0300

    add pending users button to dashboard

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,11 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    @if (auth()->user()->is_admin)
+                        <x-nav-link :href="route('pending-users')" :active="request()->routeIs('pending-users')">
+                            Pending Users
+                        </x-nav-link>
+                    @endif
                 </div>
             </div>
 
