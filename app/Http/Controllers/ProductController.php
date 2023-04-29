c2636bf1ec489853ee780492c4ea460b1424d4d9
MOohamedMahfouz <mouhamadmahfouz20@gmail.com>
2023-04-29 15:49:09 +0300

    product controller clean up commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -18,92 +18,80 @@ class ProductController extends Controller
 
 
 
-public function importProducts(Request $request)
-{
-    // Get the file from the request
-    $file = $request->file('file');
-    // Load the Excel file
-    $spreadsheet = IOFactory::load($file);
+    public function importProducts(Request $request)
+    {
+        // Get the file from the request
+        $file = $request->file('file');
+        // Load the Excel file
+        $spreadsheet = IOFactory::load($file);
 
-    // Get the first worksheet
-    $worksheet = $spreadsheet->getActiveSheet();
+        // Get the first worksheet
+        $worksheet = $spreadsheet->getActiveSheet();
 
-    $isFirstElement = 1;
-    $map = [
-        'products' => '0',
-        'type' => '1',
-        'qty' => '2',
-    ];
-    // Loop through the rows
-    foreach ($worksheet->getRowIterator() as $row) {
-        // Get the cell values
-        $cellIterator = $row->getCellIterator();
-        $cellIterator->setIterateOnlyExistingCells(FALSE);
-        $data = [];
+        $isFirstElement = 1;
+        $map = [
+            'products' => '0',
+            'type' => '1',
+            'qty' => '2',
+        ];
+        function mappingExcelField(array $data, $field,$map)
+        {
+            $fieldExsist = 0;
+            for ($i = 0; $i < 3; $i++) {
+                if ($data[$i] == $field) {
+                    $map[$field] = $i;
+                    $fieldExsist = 1;
+                }
+            }
+            if ($fieldExsist === 0) {
+                for ($i = 0; $i < 3; $i++) {
+                    if ($field == 'type') {
+                        if ($data[$i] === null && $i != $map['products']) {
+                            $map['type'] = $i;
+                            break;
+                        }
+                    } else if ($field == 'qty') {
+                        if ($data[$i] === null && $i != $map['products'] && $i != $map['type']) {
+                            $map['qty'] = $i;
+                            break;
+                        }
+                    } else if ($field == 'products') {
+                        if ($data[$i] === null) {
+                            $map[$field] = $i;
+                            break;
+                        }
+                    }
+                }
+            }
+            return $map;
+        }
+        // Loop through the rows
+        foreach ($worksheet->getRowIterator() as $row) {
+            // Get the cell values
+            $cellIterator = $row->getCellIterator();
+            $cellIterator->setIterateOnlyExistingCells(FALSE);
+            $data = [];
 
-        foreach ($cellIterator as $cell) {
-            $data[] = $cell->getValue();
+            foreach ($cellIterator as $cell) {
+                $data[] = $cell->getValue();
+            }
+
+            if ($isFirstElement) {
+                $map = mappingExcelField($data, 'products',$map);
+                $map = mappingExcelField($data, 'type',$map);
+                $map = mappingExcelField($data, 'qty',$map);
+                $isFirstElement = 0;
+                continue;
+            }
+            // Create a new product record
+            $product = new Product();
+            $product->name = $data[$map['products']];
+            $product->type = $data[$map['type']];
+            $product->qty = $data[$map['qty']];
+            $product->save();
         }
 
-        if ($isFirstElement) {
-
-            $products = 0;
-            $type = 0;
-            $qty = 0;
-            for($i = 0 ; $i < 3 ; $i++) {
-                if ($data[$i] == 'products') {
-                    $map['products'] = $i;
-                    $products = 1;
-                }
-            }
-            if ($products === 0) {
-                for($i = 0 ; $i < 3 ; $i++) {
-                    if ($data[$i] === null) {
-                        $map['products'] = $i;
-                        break;
-                    }
-                }
-            }
-            for($i = 0 ; $i < 3 ; $i++) {
-                if ($data[$i] == 'type') {
-                    $map['type'] = $i;
-                    $type = 1;
-                }
-            }
-            if ($type === 0) {
-                for($i = 0 ; $i < 3 ; $i++) {
-                    if ($data[$i] == null && $i !== $map['products']) {
-                        $map['type'] = $i;
-                        break;
-                    }
-                }
-            }
-            for($i = 0 ; $i < 3 ; $i++) {
-                if ($data[$i] == 'qty') {
-                    $map['qty'] = $i;
-                    $qty = 1;
-                }
-            }
-            if ($qty === 0) {
-                for($i = 0 ; $i < 3 ; $i++) {
-                    if ($data[$i] == null && $i !== $map['products'] && $i !== $map['type']) {
-                        $map['qty'] = $i;
-                        break;
-                    }
-                }
-            }
-            $isFirstElement = 0;
-            continue;
-        }
-        // Create a new product record
-        $product = new Product();
-        $product->name = $data[$map['products']];
-        $product->type = $data[$map['type']];
-        $product->qty = $data[$map['qty']];
-        $product->save();
+        // Return a response
+        return to_route('dashboard');
     }
-
-    // Return a response
-    return to_route('dashboard');
-}
 }
